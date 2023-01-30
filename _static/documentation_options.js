37ce57b6d324d72d3f53fd62cc1f5e565e23951e
vitorruiz <vitorruiz17@gmail.com>
2023-01-30 20:22:46 +0000

    Deploying to gh-pages from @ vitorruiz/tibia.py@29a5e79573e739971f28f442c67abdc71ad8a2a3 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.2.1',
-    LANGUAGE: 'None',
+    VERSION: '5.5.2',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
